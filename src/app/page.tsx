c2d0625388d019618544a8e606817b787ee7ec64
Shivansh Karan <96942282+SpaceTesla@users.noreply.github.com>
2025-03-04 16:43:34 +0530

    feat: add responsive layout to Home page with improved grid and flexbox styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import Image from 'next/image';
 import Paper from '../../public/paper.svg';
 import Socials from '@/components/socials';
 import Point from '@/components/point';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="h-screen pt-[88px]">
-      <div className="grid h-full min-h-full md:grid-cols-4">
+    <main className="h-[calc(100vh-5.5rem)] pt-[5.5rem]">
+      <div className="grid h-full min-h-full lg:grid-cols-4">
         {/* Left column - Benefits */}
-        <div className="col-span-1 flex h-full flex-col items-center justify-center gap-8 py-8 pl-4">
+        <div className="col-span-1 flex h-full flex-col items-start justify-center gap-8 py-8 pl-4">
           <Point
             icon={'icon'}
             heading={'Saving Water'}
@@ -30,29 +31,47 @@ export default function Home() {
               'With every ream you offset 200 Kgs of carbon footprint helping avert global warming'
             }
           />
-
-          {/* Social Media Icons */}
-          <div className="pt-8">
-            <Socials />
-          </div>
         </div>
-
         {/* Right column - Paper Image and Tagline */}
-        <div className="relative col-span-3 ml-8 flex items-center">
-          <div className="relative flex h-full w-full">
-            <div className="absolute left-0 top-1/2 -translate-y-1/2 transform">
-              <div className="relative max-h-[600px] max-w-[600px]">
-                <Image
-                  src={Paper}
-                  alt="BUTTR Paper Stack"
-                  width={600}
-                  height={600}
-                  className="object-contain"
-                />
+        <div className="relative col-span-3 flex flex-col justify-center md:ml-8 md:justify-start">
+          <div className="relative left-0 top-1/2 w-fit -translate-y-1/2 transform">
+            <div className="relative flex max-h-[37.5rem] max-w-[37.5rem] rounded-full after:absolute after:left-[50%] after:top-[50%] after:-z-10 after:h-[90%] after:w-[90%] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:bg-buttr-green">
+              <Image
+                src={Paper}
+                alt="BUTTR Paper Stack"
+                width={600}
+                height={600}
+                className="object-contain"
+              />
+              <div className="absolute inset-0 left-[6em] flex -rotate-[28deg] flex-col items-center justify-center gap-4 md:left-[8em]">
+                <Link
+                  href={'/about'}
+                  className={
+                    'text-[1em] font-black uppercase transition hover:scale-105 hover:text-buttr-green hover:drop-shadow md:text-[1.25em]'
+                  }
+                >
+                  Know More
+                </Link>
+                <Link
+                  href={'/shop'}
+                  className={
+                    'text-[1em] font-black uppercase transition hover:scale-105 hover:text-buttr-green hover:drop-shadow md:text-[1.25em]'
+                  }
+                >
+                  Shop Buttr
+                </Link>
+                <Link
+                  href={'/contact'}
+                  className={
+                    'text-[1em] font-black uppercase transition hover:scale-105 hover:text-buttr-green hover:drop-shadow md:text-[1.25em]'
+                  }
+                >
+                  Contact Us
+                </Link>
               </div>
             </div>
-            <div className="absolute left-[450px] top-[400px] text-right">
-              <h2 className="line-height-0.5 text-5xl font-bold leading-none text-gray-900">
+            <div className="relative -top-1/4 flex md:-top-1/3 md:left-24">
+              <h2 className="line-height-0.5 ml-auto px-2 text-3xl font-bold leading-none text-gray-900 md:text-5xl">
                 From Bin
                 <br />
                 To Pin
@@ -60,6 +79,11 @@ export default function Home() {
             </div>
           </div>
         </div>
+      </div>
+
+      {/* Social Media Icons */}
+      <div className="pt-8">
+        <Socials />
       </div>
     </main>
   );
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,56 +1,108 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Logo from '../../public/logo.svg';
 import Link from 'next/link';
 
 const Nav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div
-      className={'fixed flex w-full items-center justify-between bg-white p-6'}
-    >
-      <div
-        className={
-          'absolute -left-[280px] -top-[430px] z-10 h-[560px] w-[560px] rounded-full bg-buttr-green'
-        }
-      ></div>
-      <div className={'nav-left z-30'}>
-        <Link href={'/'}>
-          <Image src={Logo} alt={'logo'} width={128} height={128} />
+    <div className="fixed flex w-full items-center justify-between bg-white p-6">
+      <div className="absolute -left-[280px] -top-[430px] z-10 h-[560px] w-[560px] rounded-full bg-buttr-green"></div>
+      <div className="nav-left z-30">
+        <Link href="/">
+          <Image src={Logo} alt="logo" width={128} height={128} />
         </Link>
       </div>
-      <div className={'nav-center flex gap-8 font-medium uppercase'}>
+      <div className="nav-center hidden gap-8 font-medium uppercase md:flex">
         <Link
-          href={'/'}
-          className={'cursor-pointer transition hover:text-buttr-green'}
+          href="/"
+          className="cursor-pointer transition hover:text-buttr-green"
         >
           Home
         </Link>
         <Link
-          href={'/about'}
-          className={'cursor-pointer transition hover:text-buttr-green'}
+          href="/about"
+          className="cursor-pointer transition hover:text-buttr-green"
         >
           About Us
         </Link>
         <Link
-          href={'/store'}
-          className={'cursor-pointer transition hover:text-buttr-green'}
+          href="/store"
+          className="cursor-pointer transition hover:text-buttr-green"
         >
           Store
         </Link>
         <Link
-          href={'/blog'}
-          className={'cursor-pointer transition hover:text-buttr-green'}
+          href="/blog"
+          className="cursor-pointer transition hover:text-buttr-green"
         >
           Blog
         </Link>
       </div>
       <Link
-        href={'/contact'}
-        className={'nav-right bg-buttr-green px-4 py-2 uppercase'}
+        href="/contact"
+        className="nav-right hidden bg-buttr-green px-4 py-2 uppercase md:block"
       >
         Contact Us
       </Link>
+      <div className="z-30 md:hidden">
+        <button onClick={toggleMenu} className="focus:outline-none">
+          <svg
+            className="h-6 w-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      {isMenuOpen && (
+        <div className="absolute left-0 top-16 flex w-full flex-col items-center gap-4 bg-white py-4 md:hidden">
+          <Link
+            href="/"
+            className="cursor-pointer transition hover:text-buttr-green"
+          >
+            Home
+          </Link>
+          <Link
+            href="/about"
+            className="cursor-pointer transition hover:text-buttr-green"
+          >
+            About Us
+          </Link>
+          <Link
+            href="/store"
+            className="cursor-pointer transition hover:text-buttr-green"
+          >
+            Store
+          </Link>
+          <Link
+            href="/blog"
+            className="cursor-pointer transition hover:text-buttr-green"
+          >
+            Blog
+          </Link>
+          <Link href="/contact" className="bg-buttr-green px-4 py-2 uppercase">
+            Contact Us
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
+
 export default Nav;
