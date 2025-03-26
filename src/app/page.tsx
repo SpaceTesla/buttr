242603fe41b42662f58f30019ddc4c9e70b3459a
Shivansh Karan <96942282+SpaceTesla@users.noreply.github.com>
2025-03-26 19:15:01 +0530

    feat: ui changes to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Paper from '../../public/paper.svg';
+import Paper from '../../public/paper.png';
 import Socials from '@/components/socials';
 import Point from '@/components/point';
 import Link from 'next/link';
@@ -11,6 +11,9 @@ export const metadata = {
 };
 
 export default function Home() {
+  const paperAfterStyles =
+    'after:absolute after:left-[50%] after:top-[50%] after:-z-10 after:h-[90%] after:w-[90%] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:bg-buttr-green';
+
   return (
     <main className="h-[calc(100vh-5rem)] overflow-x-clip pt-[5rem]">
       <div className="grid h-full min-h-full lg:grid-cols-9">
@@ -43,7 +46,9 @@ export default function Home() {
         {/* Right column - Paper Image and Tagline */}
         <div className="relative col-span-6 flex flex-col justify-center md:justify-center lg:absolute lg:left-1/2 lg:top-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2 xl:max-h-[40rem] xl:max-w-[40rem]">
           <div className="relative flex h-min w-full items-center justify-center">
-            <div className="relative flex max-h-[37.5rem] max-w-[37.5rem] rounded-full after:absolute after:left-[50%] after:top-[50%] after:-z-10 after:h-[90%] after:w-[90%] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:bg-buttr-green xl:max-h-[50rem] xl:max-w-[50rem]">
+            <div
+              className={`relative flex max-h-[37.5rem] max-w-[37.5rem] rounded-full xl:max-h-[50rem] xl:max-w-[50rem]`}
+            >
               <Image
                 src={Paper}
                 alt="BUTTR Paper Stack"
@@ -56,40 +61,54 @@ export default function Home() {
                   'absolute inset-0 left-[6em] aspect-square max-h-[37.5rem] max-w-[37.5rem] overflow-clip md:left-[8em]'
                 }
               >
-                <div className="relative left-[35%] top-[50%] flex w-fit -rotate-[28deg] flex-col items-center justify-center gap-4">
-                  <Link
-                    href={'/about'}
-                    className={
-                      'text-[1em] font-black uppercase transition hover:scale-105 hover:text-buttr-green hover:drop-shadow md:text-[1.25rem]'
-                    }
+                <div
+                  className="relative left-[35%] top-[50%] flex w-fit -rotate-[25deg] flex-col items-center justify-center gap-4 tracking-wider"
+                  style={{
+                    perspective: '800px',
+                    transformStyle: 'preserve-3d',
+                  }}
+                >
+                  <div
+                    style={{
+                      transform: 'rotateY(25deg) rotateX(10deg)',
+                      transformStyle: 'preserve-3d',
+                    }}
+                    className="flex flex-col items-center gap-4"
                   >
-                    Know More
-                  </Link>
-                  <Link
-                    href={'/store'}
-                    className={
-                      'text-[1em] font-black uppercase transition hover:scale-105 hover:text-buttr-green hover:drop-shadow md:text-[1.25em]'
-                    }
-                  >
-                    Shop Buttr
-                  </Link>
-                  <Link
-                    href={'/contact'}
-                    className={
-                      'text-[1em] font-black uppercase transition hover:scale-105 hover:text-buttr-green hover:drop-shadow md:text-[1.25em]'
-                    }
-                  >
-                    Contact Us
-                  </Link>
+                    <Link
+                      href={'/about'}
+                      className={
+                        'text-[1em] font-black uppercase transition hover:scale-105 hover:text-buttr-green hover:drop-shadow md:text-[1.25rem]'
+                      }
+                    >
+                      Know More
+                    </Link>
+                    <Link
+                      href={'/store'}
+                      className={
+                        'text-[1em] font-black uppercase transition hover:scale-105 hover:text-buttr-green hover:drop-shadow md:text-[1.25em]'
+                      }
+                    >
+                      Shop Buttr
+                    </Link>
+                    <Link
+                      href={'/contact'}
+                      className={
+                        'text-[1em] font-black uppercase transition hover:scale-105 hover:text-buttr-green hover:drop-shadow md:text-[1.25em]'
+                      }
+                    >
+                      Contact Us
+                    </Link>
+                  </div>
                 </div>
               </div>
-              <div className="absolute bottom-12 right-4 flex items-end justify-start md:-right-16 md:bottom-32">
-                <h2 className="line-height-0.5 px-2 text-2xl font-bold leading-none text-gray-900 md:text-4xl xl:text-5xl">
-                  From Bin
-                  <br />
-                  To Pin
-                </h2>
-              </div>
+              {/*<div className="absolute bottom-12 right-4 flex items-end justify-start md:-right-16 md:bottom-32">*/}
+              {/*  <h2 className="line-height-0.5 px-2 text-2xl font-bold leading-none text-gray-900 md:text-4xl xl:text-5xl">*/}
+              {/*    From Bin*/}
+              {/*    <br />*/}
+              {/*    To Pin*/}
+              {/*  </h2>*/}
+              {/*</div>*/}
             </div>
           </div>
         </div>
--- src/components/design.tsx
+++ src/components/design.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import Pattern from '../../public/pattern.png';
+import Image from 'next/image';
 
 const Design = () => {
   return (
     <>
       <div
         className={
-          'fixed -bottom-[400px] -right-[400px] hidden h-[560px] w-[560px] rounded-full bg-buttr-green md:block'
+          'fixed -bottom-[400px] -right-[400px] hidden h-[560px] w-[560px] rounded-full md:block'
         }
-      />
+      >
+        <Image
+          src={Pattern}
+          alt="BUTTR Paper Stack"
+          width={800}
+          height={800}
+          className="object-contain"
+        />
+      </div>
     </>
   );
 };
